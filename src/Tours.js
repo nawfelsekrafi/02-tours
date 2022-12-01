cfa678ca8c1e218078b041a2d4a731873e50606a
Taki Academy <takiacademy@Nawfel-Sekrafi.local>
2022-12-01 15:52:39 +0100

    first commit

---
+++ src/Tours.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our tour</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour tour={tour} key={tour.id} removeTour={removeTour} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
